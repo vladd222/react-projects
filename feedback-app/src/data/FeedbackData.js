fed3765ed564321547d8d60f729f85255678cced
Vladislav Prodanchuk <vladislav.prodanchuk@fiserv.com>
2022-12-05 17:15:23 -0500

    Added some base components

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,27 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 8,
+        text: 'Some mock data for excursion feedback. The polaris trip was dope.'
+    },
+
+    {
+        id: 2,
+        rating: 7,
+        text: 'Some more mock data, the reef trip was dope.'
+    },
+
+    {
+        id: 3,
+        rating: 9,
+        text: 'Swedish massage was very relaxing.'
+    },
+
+    {
+        id: 4,
+        rating: 6,
+        text: 'No hot tubs anywhere, that was a bummer.'
+    }
+]
+
+export default FeedbackData

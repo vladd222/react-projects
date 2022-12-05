c0e1b43762f8c68c5c7f6090d128d1404e594b66
Vladislav Prodanchuk <vladislav.prodanchuk@fiserv.com>
2022-12-05 16:01:07 -0500

    Created necessary starter files for React feedback app, applied general styling and heading

---
+++ feedback-app/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Header ({ text, bgColor, textColor }) {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor
+    }
+
+    return (
+        <header style={headerStyles}>
+            <div className="container">
+                <h2>{text}</h2>
+            </div>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    text: 'Excursion Feedback',
+    bgColor: 'darkseagreen',
+    textColor: '#fff'
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string
+}
+
+export default Header

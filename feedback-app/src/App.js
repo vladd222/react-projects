1a13de452fe6d7c281fc6fa8f6edf4a24511e461
Vladislav Prodanchuk <vladislav.prodanchuk@fiserv.com>
2022-12-06 13:58:57 -0500

    Additional data displayed in UI

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -2,6 +2,7 @@ import {useState} from "react";
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
+import FeedbackStats from "./components/FeedbackStats"
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -15,6 +16,7 @@ function App() {
     return (<>
                 <Header/>
                 <div className="container">
+                    <FeedbackStats feedback={feedback}/>
                     <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
                 </div>
             </>)

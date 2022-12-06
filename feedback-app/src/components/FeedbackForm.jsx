5a753a06f23baae348f010ba1524430f5e8942fb
Vladislav Prodanchuk <vladislav.prodanchuk@fiserv.com>
2022-12-06 16:22:28 -0500

    Form components added

---
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -0,0 +1,56 @@
+import {useState} from "react";
+import Card from "./shared/Card"
+import Button from "./shared/Button"
+import RatingSelector  from "./RatingSelector";
+
+function FeedbackForm() {
+    const [feedbackText, setFeedbackText] = useState('')
+    const [rating, setRating] = useState(10)
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [displayMessage, setDisplayMessage] = useState('')
+
+    const handleFbChange = (e) => {
+        if (feedbackText === '') {
+            setBtnDisabled(true)
+            setDisplayMessage(null)
+        }
+        else if (feedbackText !== '' && feedbackText.trim().length <= 10) {
+            setBtnDisabled(true)
+            setDisplayMessage("A minimum of 10 characters is required to submit your review.")
+        }
+        else {
+          setBtnDisabled(false)
+          setDisplayMessage(null)
+        }
+
+        setFeedbackText(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (feedbackText.trim().length >= 10) {
+            const newFeedback = {
+                text,
+                rating
+            }
+        }
+    }
+
+    return (
+        <Card>
+            <form onSubmit={handleSubmit}>
+                <h2>How would you rate your service at this excursion?</h2>
+                <RatingSelector select={rating => setRating(rating)}/>
+                <div className="input-group">
+                    <input type="text" placeholder="Please include your review." onChange={handleFbChange}
+                     value={feedbackText}/>
+                    <Button type="submit" isDisabled={btnDisabled}>Submit Feedback</Button>
+                </div>
+
+                {displayMessage && <div className="message">{displayMessage}</div>}
+            </form>
+        </Card>
+    )
+}
+
+export default FeedbackForm
---
+++ feedback-app/src/components/RatingSelector.jsx
@@ -0,0 +1,58 @@
+import {useState} from "react";
+
+function RatingSelector({ select }) {
+    const [selectedRating, setSelectedRating] = useState(8)
+
+    const handleChange = (e) => {
+        setSelectedRating(+e.currentTarget.value)
+        select(+e.currentTarget.value)
+    }
+
+    return (
+        <ul className="rating">
+            <li>
+                <input type="radio" id="rate1" name="rating" value="1" onChange={handleChange} checked={selectedRating === 1}/>
+                <label htmlFor="rate1">1</label>
+            </li>
+            <li>
+                <input type="radio" id="rate2" name="rating" value="2" onChange={handleChange} checked={selectedRating === 2}/>
+                <label htmlFor="rate2">2</label>
+            </li>
+            <li>
+                <input type="radio" id="rate3" name="rating" value="3" onChange={handleChange} checked={selectedRating === 3}/>
+                <label htmlFor="rate3">3</label>
+            </li>
+            <li>
+                <input type="radio" id="rate4" name="rating" value="4" onChange={handleChange} checked={selectedRating === 4}/>
+                <label htmlFor="rate4">4</label>
+            </li>
+            <li>
+                <input type="radio" id="rate5" name="rating" value="5" onChange={handleChange} checked={selectedRating === 5}/>
+                <label htmlFor="rate5">5</label>
+            </li>
+            <li>
+                <input type="radio" id="rate6" name="rating" value="6" onChange={handleChange} checked={selectedRating === 6}/>
+                <label htmlFor="rate6">6</label>
+            </li>
+            <li>
+                <input type="radio" id="rate7" name="rating" value="7" onChange={handleChange} checked={selectedRating === 7}/>
+                <label htmlFor="rate7">7</label>
+            </li>
+            <li>
+                <input type="radio" id="rate8" name="rating" value="8" onChange={handleChange} checked={selectedRating === 8}/>
+                <label htmlFor="rate8">8</label>
+            </li>
+            <li>
+                <input type="radio" id="rate9" name="rating" value="9" onChange={handleChange} checked={selectedRating === 9}/>
+                <label htmlFor="rate9">9</label>
+            </li>
+            <li>
+                <input type="radio" id="rate10" name="rating" value="10" onChange={handleChange} checked={selectedRating === 10}/>
+                <label htmlFor="rate10">10</label>
+            </li>
+        </ul>
+    )
+
+}
+
+export default RatingSelector

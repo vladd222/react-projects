c30cd59d7895772b02ce682d363a1ab7a8527f6c
Vladislav Prodanchuk <vladislav.prodanchuk@fiserv.com>
2022-12-06 10:27:33 -0500

    Card component and conditional classes/styling

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,11 +1,18 @@
+import Card from "./shared/Card"
+import PropTypes from "prop-types";
+
 function ReviewItem({ item }) {
 
     return (
-        <div className="card">
+        <Card reverse={true}>
             <div className="num-display">{item.rating}</div>
             <div className="text-display">{item.text}</div>
-        </div>
+        </Card>
     )
+}
+
+ReviewItem.propTypes = {
+    item: PropTypes.object.isRequired
 }
 
 export default ReviewItem

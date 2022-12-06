476000a32e35102b1b20604b783c5837087b2964
Vladislav Prodanchuk <vladislav.prodanchuk@fiserv.com>
2022-12-06 11:19:47 -0500

    Applied filtering to remove feedback

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,11 +1,13 @@
+import { FaTimes } from 'react-icons/fa'
 import Card from "./shared/Card"
 import PropTypes from "prop-types";
 
-function ReviewItem({ item }) {
+function ReviewItem({ item, handleDelete }) {
 
     return (
-        <Card reverse={true}>
+        <Card>
             <div className="num-display">{item.rating}</div>
+            <button className="close" onClick={() => handleDelete(item.id)}><FaTimes color="purple"/></button>
             <div className="text-display">{item.text}</div>
         </Card>
     )

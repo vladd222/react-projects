237ba4bfb28059648698a78a590af4500e78ab82
Vladislav Prodanchuk <vladislav.prodanchuk@fiserv.com>
2022-12-08 15:05:08 -0500

    Backend server set up, data is now pulled from feedbackdb.json instead of hardcoded, added spinner component while awaiting page load

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -2,19 +2,27 @@ import FeedbackItem from "./FeedbackItem"
 import PropTypes from "prop-types";
 import {useContext} from 'react'
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner"
 
 function FeedbackList({/* feedback, handleDelete */}) {
-    const {feedback} = useContext(FeedbackContext)
-    if (!feedback || feedback.length === 0) {
+    const {feedback, isLoading} = useContext(FeedbackContext)
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return (<p>No feedback has been provided yet.</p>)
     }
 
-    return (
+    return isLoading ?<Spinner/> : (<div className="feedback-list">
+        {feedback.map((item) => (
+            <FeedbackItem key={item.id} item={item} /*handleDelete={handleDelete}*//>
+        ))}
+    </div>
+
+
+   /* return (
         <div className="feedback-list">
             {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} /*handleDelete={handleDelete}*//>
-            ))}
-        </div>
+                <FeedbackItem key={item.id} item={item} /*handleDelete={handleDelete}*////>
+    //        ))}
+    //    </div>
     )
 }
 
--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -1,35 +1,31 @@
-import {createContext, useState} from 'react'
+import {createContext, useState, useEffect} from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
+    const [isLoading, setIsLoading] = useState(true)
+
     const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This item is coming from context',
-            rating: 10
-        },
-        {
-            id: 2,
-            text: 'This item is coming from context 2',
-            rating: 8
-        },
-        {
-            id: 3,
-            text: 'This item is coming from context 3',
-            rating: 6
-        },
-        {
-            id: 4,
-            text: 'This item is coming from context 4',
-            rating: 4
-        }
+
     ])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    // Fetch feedback data from backend (mock json file)
+    const fetchFeedback = async () => {
+        const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+        const data = await response.json()
+        console.log(data)
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
     // Add a new entry (new feedback)
     const addFeedback = (newFeedback) => {
@@ -65,6 +61,7 @@ export const FeedbackProvider = ({children}) => {
         <FeedbackContext.Provider value={{
             feedback,
             feedbackEdit,
+            isLoading,
             deleteFeedback,
             addFeedback,
             editFeedback,
